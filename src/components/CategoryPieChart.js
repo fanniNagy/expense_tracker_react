3d299c74b8882d0158d06b9ddc3ee518afa7f615
fanniNagy <nagy.fanni.5@gmail.com>
2021-01-21 22:42:08 +0100

    refactor pie chart

--- src/components/CategoryPieChart.js
+++ src/components/CategoryPieChart.js
@@ -1,72 +1,45 @@
 import { ResponsivePie } from '@nivo/pie'
+import { linearGradientDef } from '@nivo/core'
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
+/*
+ data={[
+     {
+         "id": "CATEGORY",
+         "label": "CATEGORY",
+         "value": PRICE,
+     }
+ ]}
+*/
+const MyResponsivePie = ({data}) => (
 
-
-const MyResponsivePie = () => (
     <ResponsivePie
-        data={[
-            {
-                "id": "FOOD",
-                "label": "FOOD",
-                "value": 502,
-            },
-            {
-                "id": "HOUSEHOLD",
-                "label": "HOUSEHOLD",
-                "value": 74,
-            },
-            {
-                "id": "TRAVEL",
-                "label": "TRAVEL",
-                "value": 519,
-            },
-            {
-                "id": "PET",
-                "label": "PET",
-                "value": 461,
-            },
-            {
-                "id": "FUN",
-                "label": "FUN",
-                "value": 9200,
-            }
-        ]}
-
-
-
-        margin={{
-            top: 40,
-            right: 80,
-            bottom: 80,
-            left: 80
-        }}
+        data={data}
+        sortByValue={true}
+        startAngle={-360}
+        endAngle={360}
         innerRadius={0.5}
         padAngle={0.7}
         cornerRadius={3}
-
         margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-        innerRadius={0.6}
-        padAngle={0.7}
-        cornerRadius={3}
-        colors={{ scheme: 'set2' }}
-        borderWidth={1}
-        borderColor={{ from: 'color', modifiers: [ [ 'darker', '0.5' ] ] }}
-        radialLabelsSkipAngle={10}
-        radialLabelsTextColor="#333333"
-        radialLabelsLinkColor={{ from: 'color' }}
-        sliceLabelsSkipAngle={10}
-        sliceLabelsTextColor="#333333"
+        colors={{ scheme: 'brown_blueGreen' }}
+        // colors={["BlanchedAlmond", "purple", "orange", "Indigo", "Teal"]}
+        borderWidth={3}
+        borderColor={{ from: 'color', modifiers: [ [ 'opacity', '0.3' ] ] }}
+        // borderColor={{ from: 'color', modifiers: [ [ 'darker', '0.5' ] ] }}
+        radialLabelsLinkColor="#fff"
+        sliceLabelsTextColor="#fff"
+        // sliceLabelsTextColor={{ theme: 'background' }}
         defs={[
             {
                 id: 'dots',
                 type: 'patternDots',
                 background: 'inherit',
                 color: 'rgba(255, 255, 255, 0.3)',
-                size: 4,
+                size: 10,
                 padding: 1,
                 stagger: true
             },
@@ -78,68 +51,118 @@ const MyResponsivePie = () => (
                 rotation: -45,
                 lineWidth: 6,
                 spacing: 10
+            },
+            linearGradientDef('gradientA', [
+                { offset: 0, color: 'inherit' },
+                { offset: 100, color: 'inherit', opacity: 0 },
+            ]),
+            linearGradientDef('gradientB', [
+                { offset: 0, color: '#000' },
+                { offset: 100, color: 'inherit' },
+            ]),
+            // using plain object
+            {
+                id: 'gradientC',
+                type: 'linearGradient',
+                colors: [
+                    { offset: 0, color: 'inherit' },
+                    { offset: 100,  color: 'inherit', opacity: 0.7},
+                ],
             }
         ]}
         fill={[
             {
                 match: {
-                    id: 'ruby'
+                    id: 'PAYMENT'
                 },
                 id: 'dots'
-            },
-            {
+            },{
                 match: {
-                    id: 'c'
+                    id: 'ONETIME_INCOME'
+                },
+                id: 'gradientC'
+            },{
+                match: {
+                    id: 'CLOTHES'
+                },
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'ENTERTAINMENT'
+                },
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'FOOD'
                 },
                 id: 'dots'
-            },
-            {
+            }, {
                 match: {
-                    id: 'go'
+                    id: 'HEALTHCARE'
                 },
-                id: 'dots'
-            },
-            {
+                id: 'gradientC'
+            }, {
                 match: {
-                    id: 'python'
+                    id: 'HOUSEHOLD'
                 },
-                id: 'dots'
-            },
-            {
+                id: 'gradientC'
+            }, {
                 match: {
-                    id: 'scala'
+                    id: 'HOUSING'
                 },
-                id: 'lines'
-            },
-            {
+                id: 'gradientC'
+            }, {
                 match: {
-                    id: 'lisp'
+                    id: 'MISCELLANEOUS'
                 },
-                id: 'lines'
-            },
-            {
+                id: 'gradientC'
+            }, {
                 match: {
-                    id: 'elixir'
+                    id: 'PERSONAL'
                 },
-                id: 'lines'
-            },
-            {
+                id: 'gradientC'
+            }, {
                 match: {
-                    id: 'javascript'
+                    id: 'PETS'
                 },
-                id: 'lines'
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'SAVING'
+                },
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'SERVICES'
+                },
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'TRANSPORTATION'
+                },
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'UNCATEGORIZED'
+                },
+                id: 'gradientC'
+            }, {
+                match: {
+                    id: 'UTILITIES'
+                },
+                id: 'gradientC'
             }
         ]}
         legends={[
             {
-                anchor: 'bottom',
-                direction: 'row',
+                anchor: 'bottom-left',
+                direction: 'column',
                 justify: false,
                 translateX: 0,
                 translateY: 56,
                 itemsSpacing: 0,
-                itemWidth: 100,
-                itemHeight: 18,
+                itemWidth: 10,
+                itemHeight: 50,
                 itemTextColor: '#999',
                 itemDirection: 'left-to-right',
                 itemOpacity: 1,
@@ -154,7 +177,6 @@ const MyResponsivePie = () => (
                     }
                 ]
             }
-        ]}
-    />
+        ]} />
 )
 export default MyResponsivePie
