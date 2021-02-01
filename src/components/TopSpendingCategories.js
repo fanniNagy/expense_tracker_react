2aec01e01e01445f3c11b2eb8147b428497f8a44
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-01 20:58:03 +0100

    add component for top spending categories

---
+++ src/components/TopSpendingCategories.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+import entryService from "../services/EntryService";
+import "../css/Entry.css";
+
+const TopSpendingCategories = () => {
+    const [topSpending, setTopSpending] = useState([])
+
+    useEffect(() => {
+        entryService.getTopSpending()
+            .then(r => {
+                if (r.status === 200) {
+                    setTopSpending(r.data)
+                }
+            })
+    }, [])
+
+
+    return (
+        <div className="spending">
+            <span>Categories with biggest expense: </span>
+            <ol id="top5spending" className="flex-row">
+                {topSpending.length >= 3 ?
+                    topSpending.map(categoryCount =>
+                        <li key={categoryCount.category}>
+                            {categoryCount.category + " : " + categoryCount.price}</li>)
+                    : ""}
+            </ol>
+        </div>
+    )
+}
+export default TopSpendingCategories
