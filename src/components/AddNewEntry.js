5070d3c4c4a2275f93dbd0d605a711d482faffb8
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-09 16:28:13 +0100

    add MaterialUI to project

--- src/components/AddNewEntry.js
+++ src/components/AddNewEntry.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import entryService from "../services/EntryService"
-import "../css/Entry.css";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const AddNewEntry = ({counts, setCounts}) => {
 
@@ -24,18 +26,38 @@ const AddNewEntry = ({counts, setCounts}) => {
     };
 
     return <form className="add-entry-form" onSubmit={(event) => handleSubmit(event)}>
-        <label>Price: <input type="number" name="price" required={true} onChange={handleChange} value={entry.price}/>
-        </label>
-        <label>Date: <input type="date" name="date" onChange={handleChange} value={entry.date}/>
-        </label>
-        <label>Category:
-        <select id="categories" name="category" onChange={handleChange} defaultValue={"UNCATEGORIZED"}>
-            {entryService.CATEGORIES.map(category => <option key={category} value={category}>{category}</option>)}
-        </select>
-        </label>
-        <label>Name: <input type="text" name="name" onChange={handleChange} value={entry.name}/>
-        </label>
-        <button type="submit" value="Submit">Submit</button>
+        <TextField  id="price-input" className={"form-element form-input"} name="price" type="number" label="Price" variant="filled" required={true} onChange={handleChange} value={entry.price} />
+
+        <TextField id="date-input" className={"form-element form-input"} type="date" name="date" onChange={handleChange} value={entry.date} label="Outlined" variant="outlined" InputLabelProps={{shrink: true}}/>
+
+        <Autocomplete
+            id="category-input"
+            className={"form-element form-input"}
+            label="Category"
+            name="category"
+            defaultValue={"UNCATEGORIZED"}
+            options={entryService.CATEGORIES}
+            getOptionLabel={(option) => option}
+            onChange={handleCategoryAutoComplete}
+            // style={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} variant="outlined" />}
+        />
+
+        <TextField id="name-input" className={"form-element form-input"} type="text" name="name" onChange={handleChange} value={entry.name} label="Name" variant="outlined" InputLabelProps={{shrink: true}}/>
+
+
+        {/*<label>Price: <input type="number" name="price" required={true} onChange={handleChange} value={entry.price}/></label>*/}
+        {/*<label>Date: <input type="date" name="date" onChange={handleChange} value={entry.date}/></label>*/}
+
+        {/*<label>Category:*/}
+        {/*<select id="categories" name="category" onChange={handleChange} defaultValue={"UNCATEGORIZED"}>*/}
+        {/*    {entryService.CATEGORIES.map(category => <option key={category} value={category}>{category}</option>)}*/}
+        {/*</select>*/}
+        {/*</label>*/}
+
+        {/*<label>Name: <input type="text" name="name" onChange={handleChange} value={entry.name}/></label>*/}
+        <Button className={"form-element"} type="submit" value="Submit" variant="outlined" color="primary"> Submit </Button>
+        {/*<button className={"form-element"} type="submit" value="Submit">Submit</button>*/}
     </form>
 }
 export default AddNewEntry;
