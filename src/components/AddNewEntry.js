500f4dad29aca6eb650d692613cb397574cab983
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-10 00:19:49 +0100

    refactor clean code issues (unused comments, whitespaces etc.)

--- src/components/AddNewEntry.js
+++ src/components/AddNewEntry.js
@@ -28,16 +28,14 @@ const AddNewEntry = ({counts, setCounts}) => {
     const handleCategoryAutoComplete = (event, value) => {
         setEntry(prevState => ({
             ...prevState,
-            ["category"]: value
+            "category": value
         }));
     }
 
 
     return <form className="add-entry-form" onSubmit={(event) => handleSubmit(event)}>
         <TextField  id="price-input" className={"form-element form-input"} name="price" type="number" label="Price" variant="filled" required={true} onChange={handleChange} value={entry.price} />
-
         <TextField id="date-input" className={"form-element form-input"} type="date" name="date" onChange={handleChange} value={entry.date} label="Outlined" variant="outlined" InputLabelProps={{shrink: true}}/>
-
         <Autocomplete
             id="category-input"
             className={"form-element form-input"}
@@ -47,25 +45,10 @@ const AddNewEntry = ({counts, setCounts}) => {
             options={entryService.CATEGORIES}
             getOptionLabel={(option) => option}
             onChange={handleCategoryAutoComplete}
-            // style={{ width: 300 }}
             renderInput={(params) => <TextField {...params} variant="outlined" />}
         />
-
         <TextField id="name-input" className={"form-element form-input"} type="text" name="name" onChange={handleChange} value={entry.name} label="Name" variant="outlined" InputLabelProps={{shrink: true}}/>
-
-
-        {/*<label>Price: <input type="number" name="price" required={true} onChange={handleChange} value={entry.price}/></label>*/}
-        {/*<label>Date: <input type="date" name="date" onChange={handleChange} value={entry.date}/></label>*/}
-
-        {/*<label>Category:*/}
-        {/*<select id="categories" name="category" onChange={handleChange} defaultValue={"UNCATEGORIZED"}>*/}
-        {/*    {entryService.CATEGORIES.map(category => <option key={category} value={category}>{category}</option>)}*/}
-        {/*</select>*/}
-        {/*</label>*/}
-
-        {/*<label>Name: <input type="text" name="name" onChange={handleChange} value={entry.name}/></label>*/}
-        <Button className={"form-element"} type="submit" value="Submit" variant="outlined" color="primary"> Submit </Button>
-        {/*<button className={"form-element"} type="submit" value="Submit">Submit</button>*/}
+        <Button className={"form-element"} type="submit" value="Submit" variant="outlined" color="default"> Submit </Button>
     </form>
 }
 export default AddNewEntry;
