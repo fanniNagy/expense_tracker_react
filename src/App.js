1bb1eaddf0d57a59d0a8a37e379c4403822b5a4a
fanniNagy <nagy.fanni.5@gmail.com>
2021-03-08 17:00:58 +0100

    add react router to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 import Entry from "./components/Entry";
+import Login from "./components/Login"
+import {UserProvider} from "./context/UserContext";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
 
-  return (
-    <div className="App" >
-      <Entry/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <UserProvider>
+                    <Login/>
+                    {/*
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <Switch>
+                        <Route path="/">
+                            <Entry/>
+                        </Route>
+                    </Switch>*/}
+                </UserProvider>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
