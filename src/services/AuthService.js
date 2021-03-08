9dee892ae33076bebff73e8257fa97438bff2166
fanniNagy <nagy.fanni.5@gmail.com>
2021-03-08 17:02:15 +0100

    add login function to AuthService

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,2 +1,18 @@
+import React from "react";
+import axios from 'axios';
 
+const baseUrl = 'http://localhost:8080/';
+
+const login = (username, password) => {
+    let request = axios.post(`${baseUrl}login`, {
+        "username": username,
+        "password": password
+    },{withCredentials: true});
+    let response = request.then(response => response);
+    return response;
+}
+
+export default {
+    login
+}
 
